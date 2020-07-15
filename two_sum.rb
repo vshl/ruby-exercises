dbde08c8dddff01ffbd1550f5b258ecc76a8d4ad
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2020-07-15 12:49:19 +0530

    Two sum along with three sum that adds to zero

--- two_sum.rb
+++ two_sum.rb
@@ -2,11 +2,29 @@
 
 def two_sum(nums, target)
   dict = {}
+  result = []
   nums.each_with_index do |n, i|
-    return [target - n], n if dict[target - n]
+    result << [dict[target - n], i] if dict.key?(target - n)
 
     dict[n] = i
   end
+  result
 end
 
-puts two_sum([2, 11, 7, 15], 9)
+def three_sum(nums)
+  dict = {}
+  result = []
+  nums.sort!
+  nums.each_with_index do |n, i|
+    next if dict.key?(n)
+
+    two_sum(nums[i + 1..-1], 0 - n).each do |pair|
+      result << [n] + pair.map { |j| nums[i + j + 1] }
+    end
+    dict[n] = i
+  end
+  result.uniq
+end
+
+# p two_sum([2, 11, 7, 15], 9)
+p three_sum([-1, 0, 1, 2, -1, -4])
