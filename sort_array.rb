6aa2028b54b9490981985ea07b8442813766cc93
Vishal Ravi Shankar <rsh.vishal@gmail.com>
2020-10-15 13:00:17 +0530

    added merge sort

--- sort_array.rb
+++ sort_array.rb
@@ -36,8 +36,30 @@ def swap(l, r)
   @nums[l], @nums[r] = @nums[r], @nums[l]
 end
 
+def sort_array_2(nums)
+  return nums if nums.size <= 1
+
+  mid = nums.size / 2
+  left = sort_array_2(nums.slice(0...mid))
+  right = sort_array_2(nums.slice(mid...nums.size))
+  merge(left, right)
+end
+
+def merge(left, right)
+  sorted_array = []
+  while !left.empty? && !right.empty?
+    sorted_array << if left.first < right.first
+                      left.shift
+                    else
+                      right.shift
+                    end
+  end
+  sorted_array.concat(left).concat(right)
+end
+
 describe '#sort_array' do
-  it 'sorts an array in ascending order' do
+  it 'sorts an array in ascending order using quick sort' do
+    skip
     arr = [4, 3, 1, 7, 9, 5]
     expected = [1, 3, 4, 5, 7, 9]
     assert_equal sort_array(arr), expected
@@ -47,5 +69,14 @@ describe '#sort_array' do
     arr = [2]
     expected = [2]
     assert_equal sort_array(arr), expected
+  end
+end
+
+describe '#sort_array_2' do
+  it 'sorts an array using merge sort' do
+    assert_equal sort_array_2([2]), [2]
+    assert_equal sort_array_2([]), []
+    assert_equal sort_array_2([1, 3, 2]), [1, 2, 3]
+    assert_equal sort_array_2([1, 5, 3, 2, 4, 6, 9, 8, 7, 0]), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 end
